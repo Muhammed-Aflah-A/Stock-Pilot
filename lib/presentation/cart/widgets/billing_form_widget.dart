5008e36d57b729f51f97860647d57265727764f5
Muhammed Aflah <aflahathimannil@gmail.com>
2026-04-03 15:49:38 +0530

    Billing form date feild updated

--- lib/presentation/cart/widgets/billing_form_widget.dart
+++ lib/presentation/cart/widgets/billing_form_widget.dart
@@ -90,7 +90,7 @@ class _BillingFormWidgetState extends State<BillingFormWidget> {
                 context: context,
                 initialDate: DateTime.now(),
                 firstDate: DateTime(2020),
-                lastDate: DateTime(2100),
+                lastDate: DateTime.now(),
               );
               if (pickedDate != null) {
                 final formatted = DateFormat(
